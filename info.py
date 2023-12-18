232b87bea8696ce62979ee4fa97f8cf02705b196
vikaskumar133 <154025611+vikaskumar133@users.noreply.github.com>
2023-12-18 08:27:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
 # Only Give Value in LAZY_RENAMERS if you have enabled LAZY_MODE ----- !
 REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
 #   REQ_CHANNEL is for the logs of that content name which is not found in group -- !
-URL_MODE = is_enabled((environ.get("URL_MODE")), True)
+URL_MODE = is_enabled((environ.get("URL_MODE")), False)
 # Use True false in url mode => Set value true if you want shortlinks - else - use value False ----- !
 
 # URL Shortener
